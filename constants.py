20b380e6bb82cc8bfd785ac0771627cbcdeb1e36
Brandt <brandt.buchda@gmail.com>
2023-10-29 13:35:47 -0500

    Rename python files to adhere to lowercase naming scheme. added constants module for directory structure and location.

---
+++ constants.py
@@ -0,0 +1,15 @@
+class Paths:
+    ARTICLE_DIRECTORY = 'articles/'
+    CSV_DIRECTORY = 'csv/'
+
+
+class Newspapers:
+    NEW_YORK_TIMES = 'NYT'
+
+
+class Csv:
+    def __init__(self):
+        self.data = {
+            'title': 'Unknown',
+            'author': 'Unknown',
+            'paragraphs': []}
